71cc3b7495bfb089aab4aec0a979cfb24e060cd3
yeehsienq <yeehsienq@gmail.com>
2024-07-12 09:25:27 +0800

    change http endpoints for battery and camera

--- src/api/routes/battery.py
+++ src/api/routes/battery.py
@@ -1,12 +1,12 @@
 # src/api/routes/battery.py
 from fastapi import APIRouter
 from paho.mqtt.publish import single as mqtt_publish
+from database import db
 import json
 
 router = APIRouter()
 
-@router.post("/")
-async def post_battery(data: dict):
-    payload = json.dumps(data)
-    mqtt_publish('turtlebot3/battery', payload, hostname='127.0.0.1')
-    return {"status": "Battery data published to MQTT"}
+@router.get("/turtlebot3/battery", tags=["turtlebot3"])
+async def get_battery():
+    battery = await db.battery.findFirst() # gets most recently created entry for battery
+    return {"status": battery}
